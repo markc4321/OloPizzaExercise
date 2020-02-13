86a04e5bcbd8f073da3e83f66475e2ff81698c3e
Mark Clark <mark@icastles.com>
2020-02-13 09:46:31 -0500

    Renamed namespace to match solution/project

--- Program.cs
+++ Program.cs
@@ -4,8 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Script.Serialization;
 
-namespace OloSolution
+namespace OloPizzaExercise
 {
+   
     class Program
     {
         static void Main(string[] args)
